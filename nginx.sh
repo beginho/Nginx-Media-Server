5ead03d0c952a399545bb901d12b3e76ede8263f
beginho <beginjo@fakat.ba>
2016-09-03 14:34:12 +0200

    Update nginx.sh

--- nginx.sh
+++ nginx.sh
@@ -4,14 +4,12 @@ sudo apt-get install build-essential libpcre3 git libpcre3-dev libssl-dev softwa
 iptables -I INPUT -p tcp --dport 8081 -j ACCEPT
 iptables -I INPUT -p tcp --dport 1935 -j ACCEPT
 useradd -r nginx
-mkdir /var/log/nginx-rtmp/
-touch /var/log/nginx-rtmp/error.log
 sudo mkdir ~/working
 cd ~/working
 wget http://nginx.org/download/nginx-1.11.3.tar.gz
 git clone https://github.com/arut/nginx-rtmp-module.git
-git clone https://github.com/beginho/Nginx-Media-Server.git
-cp ~/working/Nginx-Media-Server/conf/nginx.txt /etc/init.d/nginx-rtmp
+git clone https://github.com/beginho/nrs.git
+cp ~/working/nrs/conf/nginx.txt /etc/init.d/nginx-rtmp
 sudo chmod +x /etc/init.d/nginx-rtmp
 sudo /usr/sbin/update-rc.d -f nginx-rtmp defaults
 tar xf nginx-1.11.3.tar.gz
@@ -20,8 +18,7 @@ cd ~/working/nginx-1.11.3
 sudo make && make install
 mkdir /opt/nginx/html/live/
 mkdir /opt/nginx/html/live/hls/
-touch /opt/nginx/conf/nginx.conf
-cp ~/working/Nginx-Media-Server/conf/nginx.conf /opt/nginx/conf/nginx.conf
+cp ~/working/nrs/conf/nginx.conf /opt/nginx/conf/nginx.conf
 ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//' )
 sed -i -- 's/replaceip/'"$ip"'/g' /opt/nginx/conf/nginx.conf
 rm -f /opt/nginx/conf/nginx.conf.default
